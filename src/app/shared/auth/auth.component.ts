348374901d24ac943ef0a54dbff31df71a341662
ashleybrooke0385 <111311541+ashleybrooke0385@users.noreply.github.com>
2022-12-01 11:21:29 -0600

    Auth SIgn In

---
+++ src/app/shared/auth/auth.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
+})
+export class AuthComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+isLoginMode = true;
+
+onSwitchAuthMode() {
+    this.isLoginMode = !this.isLoginMode;
+}
+
+onAuthFormSubmit(formObj: NgForm) {
+  console.log('Form Values:', formObj.value);
+  formObj.reset();
+
