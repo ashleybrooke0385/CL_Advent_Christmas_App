c6b359d7b2cb021a82709c23e43e366b48959080
Nathaniel Truitt <nathantruitt2@gmail.com>
2022-12-12 14:04:42 -0600

    disabling scroll for modal

--- src/app/calendar/calendar-modal/calendar-modal.component.ts
+++ src/app/calendar/calendar-modal/calendar-modal.component.ts
@@ -1,15 +1,34 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-calendar-modal',
   templateUrl: './calendar-modal.component.html',
   styleUrls: ['./calendar-modal.component.css'],
 })
-export class CalendarModalComponent {
+export class CalendarModalComponent implements OnInit, OnDestroy {
   @Input() data: any;
   @Output() closeEvent = new EventEmitter<void>();
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, 'on-modal');
+  }
 
   onClose() {
     this.closeEvent.emit();
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body, 'on-modal');
   }
 }
